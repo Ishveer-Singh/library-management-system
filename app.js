ba24e99e08f20f905bd62422a4fc87acfc38f1e2
Ishveer-Singh <ishveersingh2007@gmail.com>
2026-08-09 19:36:43 +0530

    prepare backend for deployment

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.use("/api/v1/issued-books",issuedBooksRoutes)
 
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server running on port ${process.env.PORT}`)
+// })
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- db.js
+++ db.js
@@ -1,19 +1,44 @@
-const mysql = require("mysql2")
+// const mysql = require("mysql2")
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// })
+
+// db.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("MySQL Connected");
+// });
+
+// module.exports = db;
+
+
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-})
+
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 db.connect((err) => {
     if (err) {
         console.log(err);
         return;
     }
+
     console.log("MySQL Connected");
 });
 
 module.exports = db;
-

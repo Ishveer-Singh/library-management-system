e18e7d671fd9398f77af83c35b7cb7c2e08cefad
Ishveer-Singh <ishveersingh2007@gmail.com>
2026-08-10 13:30:50 +0530

    fix: remove duplicate db export

--- db.js
+++ db.js
@@ -1,23 +1,3 @@
-// const mysql = require("mysql2")
-
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-// })
-
-// db.connect((err) => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log("MySQL Connected");
-// });
-
-// module.exports = db;
-
-
 // const mysql = require("mysql2");
 
 // const db = mysql.createConnection({
@@ -41,18 +21,21 @@
 //     console.log("MySQL Connected");
 // });
 
-module.exports = db;
 
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db;
